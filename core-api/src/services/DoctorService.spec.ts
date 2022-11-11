6d52338dd87fbb5f633b453fdb915e794d13b4e8
Bohdan <bohdan@MacBook-Pro-Bohdan.local>
2022-11-11 11:49:39 +0200

    backend fixes

--- core-api/src/services/DoctorService.spec.ts
+++ core-api/src/services/DoctorService.spec.ts
@@ -7,53 +7,76 @@ import Container from "typedi";
 import { ConnectionManager, Repository } from "typeorm";
 import { DoctorService } from "./DoctorService";
 
-const mockRepo: Partial<Repository<Availability>> = {};
+export const mockRepoAvailability: Partial<Repository<Availability>> = {};
+export const mockRepoAppointment: Partial<Repository<Appointment>> = {};
 
-describe('DoctorService', () => {
+describe("DoctorService", () => {
   beforeAll(() => {
-    Container.set(ConnectionManager, createMockRepo(mockRepo));
+    Container.set(ConnectionManager, {
+      has: () => true,
+      get: () => ({
+        getRepository: (repositoryType: any) => {
+          switch (repositoryType) {
+            case Appointment:
+              return mockRepoAppointment;
+            case Availability:
+              return mockRepoAvailability;
+            default:
+              console.warn(`No mock repository found for ${repositoryType}`);
+              return null;
+          }
+        },
+      }),
+    });
   });
 
-  describe('slots query', () => {
-    it('should return all slots for doctor', async () => {
+  describe("slots query", () => {
+    it("should return all slots for doctor", async () => {
       const doctor = new Doctor();
+      doctor.id = 1;
+      doctor.name = "doctor1";
       doctor.availability = [
         {
           dayOfWeek: 1, // Monday
-          startTimeUtc: '09:00',
-          endTimeUtc: '17:00',
+          startTimeUtc: "09:00",
+          endTimeUtc: "17:00",
         },
         {
           dayOfWeek: 2, // Tuesday
-          startTimeUtc: '09:00',
-          endTimeUtc: '12:00',
+          startTimeUtc: "09:00",
+          endTimeUtc: "12:00",
         },
         {
           dayOfWeek: 2, // Tuesday
-          startTimeUtc: '14:00',
-          endTimeUtc: '17:00',
+          startTimeUtc: "14:00",
+          endTimeUtc: "17:00",
         },
       ].map((a) => {
         const availability = new Availability();
+        availability.doctor = doctor;
         availability.dayOfWeek = a.dayOfWeek;
         availability.startTimeUtc = a.startTimeUtc;
         availability.endTimeUtc = a.endTimeUtc;
         return availability;
       });
 
-      mockRepo.find = jest.fn(() => {
+      mockRepoAvailability.find = jest.fn(() => {
         return Promise.resolve(doctor.availability);
+      });
+
+      mockRepoAppointment.find = jest.fn(() => {
+        return Promise.resolve([]);
       });
 
       const sut = Container.get(DoctorService);
 
       // from 9am next monday
       const from = setMinutes(
-        setHours(addDays(nextMonday(new Date()), 1), 9),
+        setHours(nextMonday(new Date()), 9),
         0
       );
       const to = addDays(from, 7);
-      const slots = await sut.getAvailableSlots(1, from, to);
+      const slots = await sut.getAvailableSlots(doctor.id, from, to);
 
       const slotsOnMonday = ((17 - 9) * 60) / 15;
       const slotsOnTuesdayMorning = ((12 - 9) * 60) / 15;
@@ -66,6 +89,8 @@ describe('DoctorService', () => {
 
     it('should not return slot if appointment exists for doctor', async () => {
       const doctor = new Doctor();
+      doctor.id = 1;
+      doctor.name = "doctor1";
       doctor.availability = [
         {
           dayOfWeek: 1,
@@ -84,6 +109,7 @@ describe('DoctorService', () => {
         },
       ].map((a) => {
         const availability = new Availability();
+        availability.doctor = doctor;
         availability.dayOfWeek = a.dayOfWeek;
         availability.startTimeUtc = a.startTimeUtc;
         availability.endTimeUtc = a.endTimeUtc;
@@ -97,23 +123,28 @@ describe('DoctorService', () => {
         setHours(addDays(nextMonday(new Date()), 1), 14),
         0
       );
+      appointment.doctor = doctor;
       appointment.startTime = startTime;
       appointment.durationMinutes = 15;
       doctor.appointments = [appointment];
 
-      mockRepo.find = jest.fn(() => {
+      mockRepoAvailability.find = jest.fn(() => {
         return Promise.resolve(doctor.availability);
+      });
+
+      mockRepoAppointment.find = jest.fn(() => {
+        return Promise.resolve([appointment]);
       });
 
       const sut = Container.get(DoctorService);
 
       // from 9am next monday
       const from = setMinutes(
-        setHours(addDays(nextMonday(new Date()), 1), 9),
+        setHours(nextMonday(new Date()), 9),
         0
       );
       const to = addDays(from, 7);
-      const slots = await sut.getAvailableSlots(1, from, to);
+      const slots = await sut.getAvailableSlots(doctor.id, from, to);
 
       const slotsOnMonday = ((17 - 9) * 60) / 15;
       const slotsOnTuesdayMorning = ((12 - 9) * 60) / 15;
--- core-api/test/mocks/mockRepo.ts
+++ core-api/test/mocks/mockRepo.ts
@@ -1,4 +1,10 @@
-import { Repository } from 'typeorm';
+import { Appointment } from "@/entities/Appointment";
+import { Repository } from "typeorm";
+import {
+  mockRepoAppointment,
+  mockRepoAvailability,
+} from "@/services/DoctorService.spec";
+import { Availability } from "@/entities/Availability";
 
 const createMockRepo = (mockRepository: Partial<Repository<any>>) => {
   return {
@@ -7,6 +13,17 @@ const createMockRepo = (mockRepository: Partial<Repository<any>>) => {
       getRepository: (name: string) => {
         console.log(name);
         return mockRepository;
+      },
+      getCustomRepository: (repositoryType: any) => {
+        switch (repositoryType) {
+          case Repository<Appointment>:
+            return mockRepoAppointment;
+          case Repository<Availability>:
+            return mockRepoAvailability;
+          default:
+            console.warn(`No mock repository found for ${repositoryType}`);
+            return null;
+        }
       },
     }),
   };
