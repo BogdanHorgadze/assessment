77ceaf466196a409af3e7f19b4eb42bafa44554f
Bohdan <bohdan@MacBook-Pro-Bohdan.local>
2022-11-11 12:16:12 +0200

    fixes

--- core-api/test/mocks/mockRepo.ts
+++ core-api/test/mocks/mockRepo.ts
@@ -1,10 +1,4 @@
-import { Appointment } from "@/entities/Appointment";
 import { Repository } from "typeorm";
-import {
-  mockRepoAppointment,
-  mockRepoAvailability,
-} from "@/services/DoctorService.spec";
-import { Availability } from "@/entities/Availability";
 
 const createMockRepo = (mockRepository: Partial<Repository<any>>) => {
   return {
@@ -13,17 +7,6 @@ const createMockRepo = (mockRepository: Partial<Repository<any>>) => {
       getRepository: (name: string) => {
         console.log(name);
         return mockRepository;
-      },
-      getCustomRepository: (repositoryType: any) => {
-        switch (repositoryType) {
-          case Repository<Appointment>:
-            return mockRepoAppointment;
-          case Repository<Availability>:
-            return mockRepoAvailability;
-          default:
-            console.warn(`No mock repository found for ${repositoryType}`);
-            return null;
-        }
       },
     }),
   };
